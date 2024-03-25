4a28273f36bf9814d3587517086b89c1eb87a9af
Samiullah <samiullahtufail181@gmail.com>
2024-03-25 12:12:56 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+#! src/bin/env node
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        name: "firstNumber",
+        message: "enter first number",
+        type: "number"
+    }, {
+        name: "secondNumber",
+        message: "enter second number",
+        type: "number"
+    }, {
+        name: "operator",
+        message: "Select Operator",
+        type: "list",
+        choices: ["Add", "Subtract", "Multiply", "Divide"]
+    }
+]);
+console.log(answers);
+let result;
+if (answers.operator === "Add") {
+    result = answers.firstNumber + answers.secondNumber;
+}
+else if (answers.operator === "Subtract") {
+    result = answers.firstNumber - answers.secondNumber;
+}
+else if (answers.operator === "Multiply") {
+    result = answers.firstNumber * answers.secondNumber;
+}
+else {
+    result = answers.firstNumber / answers.secondNumber;
+}
+console.log(`"Result is " ${result}`);
